f3a0d1694e8eb3e41dc7241e079e6407e1d8d9a1
Vineet <infinite2202@gmail.com>
2025-05-24 16:39:39 +0530

    app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,15 @@
+import { useState } from "react";
+import FeedbackForm from "./components/FeedbackForm";
+import FeedbackList from "./components/FeedbackList";
+
 const App = () => {
-  return <section className="h-screen w-full bg-gray-300">App</section>;
+  const [refresh, setRefresh] = useState(false);
+
+  const handleRefresh = () => setRefresh(!refresh);
+  return <section className="h-screen w-full bg-gray-300">App
+  <FeedbackForm onFeedbackSubmit={handleRefresh} />
+  <FeedbackList key={refresh}/>
+  </section>;
 };
 
 export default App;
--- Frontend/src/components/FeedbackForm.jsx
+++ Frontend/src/components/FeedbackForm.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import { useState } from "react";
 
-const FeedbackForm = () => {
+const FeedbackForm = ({ onFeedbackSubmit }) => {
+  const [formData, setFormDate] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [success, setSuccess] = useState("");
+
+  const handleChange = (e) => {
+    setFormDate({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.name || !formData.email || !formData.message) {
+      alert("All field are required!");
+      return;
+    }
+
+    const res = await fetch("http://localhost:8000/feedback", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData),
+    });
+
+    if (res.ok) {
+      setSuccess("Feedback submitted successfully!");
+      setFormDate({ name: "", email: "", message: "" });
+      onFeedbackSubmit();
+      setTimeout(() => setSuccess(""), 3000);
+    }
+  };
   return (
-    <div>FeedbackForm</div>
-  )
-}
+    <section>
+      <form onSubmit={handleSubmit}>
+        <h2>Submit Your Feedback</h2>
+        {success && <p>{success}</p>}
+        <input
+          name="name"
+          placeholder="Name"
+          value={formData.name}
+          onChange={handleChange}
+        />
 
-export default FeedbackForm
+        <input
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+
+        <textarea
+          name="message"
+          placeholder="Your Feedback"
+          value={formData.message}
+          onChange={handleChange}
+        />
+
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
+};
+
+export default FeedbackForm;
