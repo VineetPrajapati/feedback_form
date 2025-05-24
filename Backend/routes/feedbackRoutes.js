206f7d71ab23843f7c56df190e4baf659db616cb
Vineet <infinite2202@gmail.com>
2025-05-24 16:40:31 +0530

    conditons

--- Backend/routes/feedbackRoutes.js
+++ Backend/routes/feedbackRoutes.js
@@ -6,7 +6,10 @@ const router = express.Router();
 router.post("/", async (req, res) => {
   try {
     const { name, email, message } = req.body;
-    const feedback = Feedback({ name, email, message });
+    if(!name || !email || !message){
+      return res.status(400).json({error: "ALl field are required!"})
+    }
+    const feedback = new Feedback({name, email, message});
     await feedback.save();
     res.status(201).json({message: "Feedback submitted successfully!"})
   } catch (err) {
@@ -19,7 +22,8 @@ router.get("/", async (req, res) => {
     const feedbackList = await Feedback.find().sort({createdAt: -1});
     res.json(feedbackList);
   } catch (err) {
-    res.status(500).json({ err: "Error fetching feedback" });
+    console.error(`Error fetching feedback: ${err}`)
+    res.status(500).json({ err: "Server error while fetching feedback" });
   }
 });
 
