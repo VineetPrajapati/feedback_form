9ba331eac76ff5a389e151a2713a1dda89996780
Vineet <infinite2202@gmail.com>
2025-05-24 14:30:52 +0530

    get

--- Backend/routes/feedbackRoutes.js
+++ Backend/routes/feedbackRoutes.js
@@ -4,13 +4,22 @@ import Feedback from "../models/Feedback.js";
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-    try {
-        const {name, email, message } = req.body;
-        const feedback = Feedback({name, email, message});
-        await feedback.save();
-    } catch (err) {
-        res.status(400).json({err: "Error saving feedback"});
-    }
-    
-})
+  try {
+    const { name, email, message } = req.body;
+    const feedback = Feedback({ name, email, message });
+    await feedback.save();
+  } catch (err) {
+    res.status(400).json({ err: "Error saving feedback" });
+  }
+});
 
+router.get("/", async (req, res) => {
+  try {
+    const feedbackList = await Feedback.find().sort({createdAt: -1});
+    res.json(feedbackList);
+  } catch (err) {
+    res.status(500).json({ err: "Error fetching feedback" });
+  }
+});
+
+export default router;
